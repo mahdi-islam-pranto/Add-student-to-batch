099ff0cd5f6dc3f25f4ea572ad801e578fa3b70b
mahdi-islam-pranto <mahdiprantoblog@gmail.com>
2024-07-09 18:43:32 +0600

    making the structure

--- index.php
+++ index.php
@@ -3,11 +3,14 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>my clients</title>
+    <title>batch</title>
 
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css">
 </head>
 <body>
-    
+    <center>
+    <h1>this is a coaching batch</h1>
+    </center>
+   
 </body>
 </html>
