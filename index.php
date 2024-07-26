b55b36f092580448070d839c1186950f40b46c4d
mahdi-islam-pranto <mahdiprantoblog@gmail.com>
2024-07-26 23:56:09 +0600

    student table creation with all styles

--- index.php
+++ index.php
@@ -6,44 +6,64 @@
     <title>batch</title>
 
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
-
+    <link rel="stylesheet" href="style.css">
 </head>
 <body>
-    <center>
-    <h1>this is a coaching batch</h1>
-    </center>
+
+    
+    <div id="heading">
+    <h1 >STUDY-HUB Coaching Center</h1>
+    </div>
+
+
+    
 
     <!-- student table -->
 
     <div class="container mt-5">
 
-
-    <table class="table table-striped">
+    <h3>LIST OF STUDENTS:</h3>
+    <table class="table table-bordered table-hover table-striped">
   <thead>
     <tr>
-      <th scope="col">#</th>
-      <th scope="col">First</th>
-      <th scope="col">Last</th>
-      <th scope="col">Handle</th>
+      <th scope="col">id</th>
+      <th scope="col">First Name</th>
+      <th scope="col">Last Name</th>
+      <th scope="col">Class</th>
+      <th scope="col">Batch</th>
+      <th scope="col">Phone</th>
     </tr>
   </thead>
   <tbody>
     <tr>
-      <th scope="row">1</th>
+    <td>1</td>
       <td>Mark</td>
       <td>Otto</td>
       <td>@mdo</td>
+      <td>@mdo</td>
+      <td>@mdo</td>
     </tr>
+
+
     <tr>
-      <th scope="row">2</th>
+
+        <td>2</td>
       <td>Jacob</td>
       <td>Thornton</td>
       <td>@fat</td>
+      <td>@fat</td>
+      <td>@fat</td>
     </tr>
+
+
     <tr>
-      <th scope="row">3</th>
-      <td colspan="2">Larry the Bird</td>
-      <td>@twitter</td>
+
+        <td>3</td>
+      <td>Jacob</td>
+      <td>Thornton</td>
+      <td>@fat</td>
+      <td>@fat</td>
+      <td>@fat</td>
     </tr>
   </tbody>
 </table>
